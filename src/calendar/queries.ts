3e784805776f68ef2639f21dc91a0a70ae59fe2a
lam.ho <holam9120@gmail.com>
2024-07-25 18:36:07 +0700

    update logic for filters

--- src/calendar/queries.ts
+++ src/calendar/queries.ts
@@ -4,10 +4,28 @@ import data from "@/assets/data.json";
 
 const milestones = data as unknown as BriefEvent[];
 
-export function useBrief(params?: string) {
+export function useBrief(params?: {
+  projectId?: string;
+  status?: number;
+  shouldHideFilteredTimelines?: boolean;
+}) {
   if (!params) return milestones;
 
-  return data as unknown as BriefEvent[];
+  const { shouldHideFilteredTimelines, projectId, status } = params;
+
+  return structuredClone(milestones).filter((m) => {
+    if (projectId && m.projectId !== projectId) return false;
+
+    if (status) {
+      if (shouldHideFilteredTimelines && m.status !== status) {
+        m.isHidden = true;
+      } else if (m.status !== status) {
+        return false;
+      }
+    }
+
+    return true;
+  });
 }
 
 export function useBriefList() {
@@ -15,14 +33,5 @@ export function useBriefList() {
     createdByUserId: m.project.createdByUserId,
     value: m.projectId,
     label: m.project.name,
-  }));
-}
-
-export function useMilestones(projectId?: string) {
-  if (!projectId) return [];
-
-  return milestones.map((m) => ({
-    value: m.id,
-    label: m.milestone,
   }));
 }
